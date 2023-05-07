4da65c2fc43b4587731b001ebbc3e53c43751c84
Neviton Viana <nevitonviana1@homail.com>
2023-05-07 19:24:01 -0300

    criação do projeto lista tarefas, e configuração do modular

---
+++ android/app/src/main/kotlin/com/listaTarefas/nevitonViana/lista_tarefas/MainActivity.kt
@@ -0,0 +1,6 @@
+package com.listaTarefas.nevitonViana.lista_tarefas
+
+import io.flutter.embedding.android.FlutterActivity
+
+class MainActivity: FlutterActivity() {
+}
---
+++ lib/app/app_module.dart
@@ -0,0 +1,11 @@
+import 'package:flutter_modular/flutter_modular.dart';
+
+class AppModule extends Module{
+
+  @override
+  List<Bind<Object>> get binds => [];
+
+  @override
+  List<ModularRoute> get routes => [];
+
+}
